1e76855d0623eadb3e5bcff3f52c78e67de3101b
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-03-03 10:01:10 +0530

    Removed oauth2, updated migrations

---
+++ pkg/postgres/migrations/0001_create-person,token.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE person (
+  id UUID NOT NULL PRIMARY KEY,
+  source VARCHAR(128),
+  name VARCHAR(128),
+  email VARCHAR(128),
+  picture_url VARCHAR(512),
+  deleted BOOLEAN DEFAULT false,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE token (
+  id UUID NOT NULL PRIMARY KEY,
+  person UUID NOT NULL REFERENCES person(id),
+  source VARCHAR(128),
+  access_token VARCHAR(256),
+  external_access_token VARCHAR(256),
+  external_refresh_token VARCHAR(256),
+  external_expires_in INT,
+  external_token_type VARCHAR(128),
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW()
+);
--- pkg/server/router.go
+++ pkg/server/router.go
@@ -27,24 +27,6 @@ func Router(ctx *appcontext.AppContext, db *sqlx.DB) http.Handler {
 	// PUT .../v1/users/15 access_token=tkn&name=name&deleted=true
 	// DELETE .../v1/users/15
 
-	// AppGroup
-	// POST .../v1/appGroup name=name
-	// GET .../v1/appGroup/15
-	// PUT .../v1/appGroup/15 name=name
-	// DELETE .../v1/appGroup/15
-
-	// App
-	// POST ../v1/app appGroup=appGroup&name=name&bundleId=bundle_id&platform=platform
-	// GET .../v1/app/15
-	// PUT .../v1/app/15 name=name
-	// DELETE .../v1/app/15
-
-	// Access
-	// POST ../v1/access person=person&appGroup=app_group&app=app&access_level=admin&access_given_by=person
-	// GET ../v1/access/15
-	// PUT ../v1/access/15 access_level=admin
-	// DELETE ../v1/access/15
-
 	return router
 }
 
