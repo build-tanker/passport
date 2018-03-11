f552ee99edd3cddfa0aa00025ba8ddf1513ffcdb
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-03-11 12:39:03 +0700

    [sudhanshu] removed lint warnings for responses and handler

--- pkg/common/responses/responses.go
+++ pkg/common/responses/responses.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// WriteJSON encodes the JSON
 func WriteJSON(w http.ResponseWriter, status int, i interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
@@ -15,17 +16,20 @@ func WriteJSON(w http.ResponseWriter, status int, i interface{}) {
 	// fmt.Printf(b.String())
 }
 
+// Response defines the standard output response
 type Response struct {
 	Data    interface{}     `json:"data,omitempty"`
 	Success string          `json:"success,omitempty"`
 	Errors  []ErrorResponse `json:"errors,omitempty"`
 }
 
+// ErrorResponse defines the standard error response
 type ErrorResponse struct {
 	Code    string `json:"code,omitempty"`
 	Message string `json:"message,omitempty"`
 }
 
+// NewErrorResponse writes a new error to the response
 func NewErrorResponse(code string, message string) *Response {
 	return &Response{
 		Success: "false",
--- pkg/pings/handler.go
+++ pkg/pings/handler.go
@@ -6,10 +6,13 @@ import (
 	"github.com/build-tanker/passport/pkg/common/responses"
 )
 
+// HTTPHandler type is a function which can be used an a handler
 type HTTPHandler func(w http.ResponseWriter, r *http.Request)
 
+// PingHandler is a structure to handle ping and related functionality
 type PingHandler struct{}
 
+// Ping handles the /ping request
 func (p *PingHandler) Ping() HTTPHandler {
 	return func(w http.ResponseWriter, r *http.Request) {
 		responses.WriteJSON(w, http.StatusOK, responses.Response{Success: "pong"})
