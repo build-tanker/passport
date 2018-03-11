022af10857e81ac14c9ebf04e2eed8f40e744396
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-03-11 21:47:21 +0700

    [sudhanshu] changed tests to try again

--- pkg/common/config/config_test.go
+++ pkg/common/config/config_test.go
@@ -9,6 +9,6 @@ import (
 
 func TestConfigValues(t *testing.T) {
 	conf := config.New([]string{"./testutil"})
-	assert.Equal(t, "dbname=passport user=passport password='passport' host=localhost port=5432 sslmode=disable", conf.ConnectionString())
-	assert.Equal(t, "postgres://passport:passport@localhost:5432/passport?sslmode=disable", conf.ConnectionURL())
+	assert.Equal(t, "dbname=passportTest user=passportTest password='passportTest' host=localhost port=5432 sslmode=disable", conf.ConnectionString())
+	assert.Equal(t, "postgres://passportTest:passportTest@localhost:5432/passportTest?sslmode=disable", conf.ConnectionURL())
 }
