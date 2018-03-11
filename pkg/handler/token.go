b65be5c346c7f8ce923be764f5da44f5d54e019e
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-03-11 16:44:43 +0700

    [sudhanshu] handler also has visibility into token

--- pkg/handler/token.go
+++ pkg/handler/token.go
@@ -3,21 +3,17 @@ package handler
 import (
 	"net/http"
 
-	"github.com/jmoiron/sqlx"
-
-	"github.com/build-tanker/passport/pkg/common/config"
 	"github.com/build-tanker/passport/pkg/common/responses"
 	"github.com/build-tanker/passport/pkg/token"
-	"github.com/gorilla/mux"
 )
 
 type tokenHandler struct {
 	tokens *token.Service
 }
 
-func newTokenHandler(conf *config.Config, db *sqlx.DB) *tokenHandler {
+func newTokenHandler(service *token.Service) *tokenHandler {
 	return &tokenHandler{
-		tokens: token.New(conf, db),
+		tokens: service,
 	}
 }
 
@@ -33,17 +29,4 @@ func (t *tokenHandler) add() httpHandler {
 			Success: "true",
 		})
 	}
-}
-
-func (t *tokenHandler) parseKeyFromQuery(r *http.Request, key string) string {
-	value := ""
-	if len(r.URL.Query()[key]) > 0 {
-		value = r.URL.Query()[key][0]
-	}
-	return value
-}
-
-func (t *tokenHandler) parseKeyFromVars(r *http.Request, key string) string {
-	vars := mux.Vars(r)
-	return vars[key]
 }
